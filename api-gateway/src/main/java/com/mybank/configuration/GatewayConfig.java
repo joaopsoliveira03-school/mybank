8faf3de59dc982c48e18a58813d2f116c9baf1f5
João Oliveira <joaopsoliveira03@protonmail.com>
2024-11-05 10:23:27 +0000

    Docker Backup

--- api-gateway/src/main/java/com/mybank/configuration/GatewayConfig.java
+++ api-gateway/src/main/java/com/mybank/configuration/GatewayConfig.java
@@ -1,4 +1,4 @@
-package com.mybank;
+package com.mybank.configuration;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
